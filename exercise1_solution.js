59ac7d16166f1f1f1f502b234f3b54eb17d7f0d9
Johnathan Niziol <35515+jniziol@users.noreply.github.com>
2020-11-10 10:45:23 -0600

    Add files via upload

---
+++ exercise1_solution.js
@@ -0,0 +1,36 @@
+// Q1
+
+const admin;
+const name = `John`;
+admin = name;
+
+// Q2
+
+const ourPlanet = "earth";
+
+const visitorInfo, currentUser, siteGuest, webViewer;
+
+// Q3
+
+console.log("" + 1 + 0); // "10"
+console.log("" - 1 + 0); // -1
+console.log(true + false); // 1
+console.log(6 / "3"); // 2
+console.log("2" * "3"); // 6
+console.log(+ 5 + "px"); // "5px"
+console.log("$" + 4 + 5); // "$45"
+console.log("4" - 2); // 2
+console.log("4px" - 2); // NaN
+console.log(7 / 0); // Infinity
+console.log(" -9 " + 5); // " -9 5"
+console.log(" -9 " - 5); // -14
+console.log(null + 1); // 1
+console.log(undefined + 1) // NaN
+console.log(" \t \n" - 2); // -2
+
+
+// Q4 
+2 + 10 - 19 + 4 - 90 + 1 // -92
+-20 + -19 - (-10) - (-1) + 24 // -4
+(10/5) * 4 - 20 // - 12
+4096 % 12 // 4
